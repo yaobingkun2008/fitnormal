0f3f6565ee7ea6f6acad756a4654b3dae3d3fc04
huoniao <yaobingkun2008@126.com>
2018-03-16 20:09:24 +0800

    前面用了4次插值做修正

--- src/helloworld/test.java
+++ src/helloworld/test.java
@@ -16,9 +16,12 @@ import org.apache.commons.math3.fitting.GaussianCurveFitter;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.apache.commons.math3.fitting.WeightedObservedPoint;
 import org.apache.commons.math3.fitting.WeightedObservedPoints;
+import org.apache.commons.math3.fitting.PolynomialCurveFitter;
 
 import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Vector;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -31,12 +34,34 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 public class test {
 	
+	static float poly(int high_rate,double[] xishu,double x)
+	{
+		float a = 0;
+		for(int i=0;i<=high_rate;i++)
+		{
+			a = (float)(a+ xishu[i]*Math.pow(x, i));
+		}
+		return a;
+	}
+	
 	static float normpdf(float x,double mu,double delta,float xishu)//正态分布函数
 	{
 		double r = -((Math.pow(x-mu,2))/(2*Math.pow(delta,2)));
     	return xishu*(float) (Math.pow(Math.E,r)/(Math.pow(2*Math.PI, 0.5)*delta));
 	}
 
+	static double[] polyfit(int degree,Vector<Double> x,Vector<Double> y)
+	{
+		PolynomialCurveFitter polynomialCurveFitter = PolynomialCurveFitter.create(degree);
+		ArrayList weightedObservedPoints = new ArrayList(); 
+    	for(int i=0;i<x.size();i++)
+    	{
+    		WeightedObservedPoint weightedObservedPoint = new WeightedObservedPoint(1, x.get(i) ,y.get(i) );
+            weightedObservedPoints.add(weightedObservedPoint);
+    	}
+    	return polynomialCurveFitter.fit(weightedObservedPoints);
+	}
+	
 	static void read_from_file(File x,Vector<String> a) throws IOException
 	{
 		InputStreamReader readx = new InputStreamReader(new FileInputStream(x),"GBK");
@@ -84,23 +109,40 @@ public class test {
 		
 		for(int q=30;q<=300;q=q+10)
 		{
-			WeightedObservedPoints obs = new WeightedObservedPoints();
-        	for(int i=0;i<q;i=i+10)
-        	{
-        		obs.add(Double.parseDouble(x1.get(i)),Double.parseDouble(y1.get(i)));
-        	}
-        	double[] parameters = GaussianCurveFitter.create().fit(obs.toList());
-        	//System.out.println(q/10+":");
-        	float c = normpdf((float)q,parameters[1],parameters[2],10000);
-        	file.write((String.valueOf(c)+",").getBytes());
-        	if(q==300)
-        	{
-        		for(int g=310;g<600;g=g+10)
-        		{
-        			float n = normpdf((float)g,parameters[1],parameters[2],10000);
-                	file.write((String.valueOf(n)+",").getBytes());
-        		}
-        	}
+			if(q<=200)//在q<200之前用多项式拟合
+			{
+				Vector<Double> x = new Vector<Double>();
+				Vector<Double> y = new Vector<Double>();
+				for(int k=0;k<q;k=k+5)
+				{
+					x.add(Double.parseDouble(x1.get(k)));
+					y.add(Double.parseDouble(y1.get(k)));
+				}
+				double[] xishu = polyfit(4,x,y);
+				float n = poly(4,xishu,q);
+				file.write((String.valueOf(n)+",").getBytes());
+			}
+			else
+			{
+				WeightedObservedPoints obs = new WeightedObservedPoints();
+	        	for(int i=0;i<q;i=i+10)
+	        	{
+	        		obs.add(Double.parseDouble(x1.get(i)),Double.parseDouble(y1.get(i)));
+	        	}
+	        	double[] parameters = GaussianCurveFitter.create().fit(obs.toList());
+	        	//System.out.println(q/10+":");
+	        	float c = normpdf((float)q,parameters[1],parameters[2],10000);
+	        	file.write((String.valueOf(c)+",").getBytes());
+	        	if(q==300)
+	        	{
+	        		for(int g=310;g<600;g=g+10)
+	        		{
+	        			float n = normpdf((float)g,parameters[1],parameters[2],10000);
+	                	file.write((String.valueOf(n)+",").getBytes());
+	        		}
+	        	}
+			}
+			
         	//System.out.print("\n");
 		}
         
